05db7cb108335c4a802a044e0b75c44f2b551ed2
kahansali1 <83342439+kahansali1@users.noreply.github.com>
2023-09-19 16:29:33 +0200

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,59 @@
 import logo from './assets/logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { fetchTracks } from './lib/fetchTracks';
+import { useQuery } from '@tanstack/react-query';
 
+const trackUrls = [
+  'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
+  'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
+  'https://p.scdn.co/mp3-preview/31f65b6a613010f22316c7be335b62226cf2f263',
+  'https://p.scdn.co/mp3-preview/0f6b8a3524ec410020457da4cdd7717f9addce2f',
+  'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
+];
 const App = () => {
+  const { data: tracks } = useQuery({
+    queryKey: ['tracks'],
+    queryFn: fetchTracks,
+  });
+
+  /*const AlbumCover = ({ track }) => {
+    const src = 'https://example.com/image.png'; // A changer ;)
+    return <img src={src} style={{ width: 400, height: 400 }} />;
+  };*/
+
+  //const tracksUrls = tracks.map(track => track.track.preview_url);
+  console.log(tracks);
+
+  if (tracks === undefined) {
+    return <div> Loading </div>;
+  }
+
+  const [trackIndex, setTrackIndex] = useState(0);
+  const goToNextTrack = () => {
+    setTrackIndex(trackIndex + 1);
+  };
+
+  const AlbumCover = ({ track }) => {
+    return <img src={track} style={{ width: 400, height: 400 }} />;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Bienvenue sur le blind test</h1>
       </header>
+
       <div className="App-images">
         <p>Il va falloir modifier le code pour faire un vrai blind test !!!</p>
       </div>
       <div className="App-buttons"></div>
+      <AlbumCover track={tracks[trackIndex].track.album.images[0].url} />
+      <audio src={trackUrls[trackIndex]} autoPlay controls />
+      <button onClick={goToNextTrack}>Next track</button>
+
+      <div> {trackIndex} </div>
     </div>
   );
 };
--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,5 +1,19 @@
-const apiToken: string = '';
+import { SavedTrack } from 'spotify-types';
 
-export const fetchTracks = async () => {
-  // This function does nothing, yet
+const apiToken =
+  'BQDA-LDyDnaPSeymqOnlzTOCpnSqupaPm9L4vWzdebvPvLoQ9iAwAt1hat1-SES46BjMEsHOuLQh57kSuFIxcxisS3vMIKw93HffxwUurzn3286VmLwWEFkPcQKvfkJux_36dfstwy0Iv3ZQp5D8IoUPIvid09_FPvRl21N7AqGuMkJyaPE6NCgBR-vlapVPI21jFnBHBI8rDuh2nqEHZ1FXOK71EQ';
+
+export const fetchTracks = async (): Promise<SavedTrack[]> => {
+  const response = await fetch('https://api.spotify.com/v1/me/tracks', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + apiToken,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`Fetching tracks failed with status ${response.status}`);
+  }
+  const data = (await response.json()) as { items: SavedTrack[] };
+
+  return data.items;
 };
